712dc0a19abb62ab7a8ed08ebb49e5ec5f1b8c0e
saadfromcachy <primecoder8@gmail.com>
2026-09-06 11:34:44 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+
+/*
+  Create a basic application window using your chosen windowing library.
+
+Testing input:
+  Window size: 1280 × 720
+  Title: Vulkan Renderer
+  The window should open and remain responsive until you close it.
+*/
+
+
+int main () {
+}
